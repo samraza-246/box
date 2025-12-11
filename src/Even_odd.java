016352d6f4548276f86f64a1e9e0221602afc9e5
samraza-246 <srk667460@gmail.com>
2025-12-11 17:04:20 +0530

    first commit ok

--- src/Even_odd.java
+++ src/Even_odd.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Even_odd {
+    public static void main(String[] args)
+    {
+        int num;
+        int even=0;
+        int odd=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 10 numbers:");
+        for(int i=1;i<=10;i++)
+        {
+            num=sc.nextInt();
+            if (num % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        System.out.println("Number of even numbers: "+even);
+        System.out.println("Number of odd numbers: "+odd);
+    }
 }
